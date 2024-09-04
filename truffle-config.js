b9adc5afbcfbf1b01402dff4d03a2030d5b1a8bf
Shreya Shree <shreyaashree.17@gmail.com>
2024-09-04 22:15:21 +0530

    updated

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,34 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const infuraKey = "YOUR_INFURA_PROJECT_ID";
-const mnemonic = "YOUR_METAMASK_MNEMONIC";
-
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
-    },
-    rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
-      network_id: 4,
-      gas: 4500000,
-      gasPrice: 10000000000, // 10 gwei
-    },
+      port: 7545, // Default Ganache port
+      network_id: "5777" // Match any network id
+    }
   },
-  // other configurations...
-};
+  rinkeby: {
+    provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+    network_id: 4,
+    gas: 4500000,
+    gasPrice: 10000000000, // 10 gwei
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.19",    // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {           // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "istanbul"
+      }
+    }
+  }
+}
